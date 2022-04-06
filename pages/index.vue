8a6edf77030146b78954165c1009380d25fee5b2
kdw <1197929159@qq.com>
2022-04-06 18:39:58 +0800

    初始化项目

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>首页2</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ pages/test/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>dklglkjl</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
